3dae84b7c2be440c453ef33cfa1eaa901abfd95c
nikmakpak <nicement28@gmail.com>
2023-10-07 10:07:11 +0300

    feat: add answer post

--- school_homework_board/homework_board/admin.py
+++ school_homework_board/homework_board/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Homework, Answer
 
-admin.site.register(Homework)
-admin.site.register(Answer)
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subject', 'teacher', 'due_date')
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'date_created')
+
+admin.site.register(Homework, HomeworkAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- school_homework_board/homework_board/views.py
+++ school_homework_board/homework_board/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Homework, Answer
+from .forms import AnswerForm
 
 def student_homework(request):
     homework = Homework.objects.all()
-    return render(request, 'student_homework.html', {'homework': homework})
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.student = request.user
+            instance.save()
+            return redirect('student_homework')
+    else:
+        form = AnswerForm()
+    return render(request, 'student_homework.html', {'homework': homework, 'form': form})
 
 def teacher_answers(request):
     answers = Answer.objects.all()
